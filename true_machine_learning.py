c961d646c46e782d14a525190724f812c1dce805
Smitchieyo <sam.a.bigwood@gmail.com>
2026-07-31 15:10:10 +0100

    graph

--- true_machine_learning.py
+++ true_machine_learning.py
@@ -21,3 +21,7 @@ dataset = read_csv(url, names=names)
 print(dataset.shape)
 print(dataset.head(20))
 print(dataset.describe())
+print(dataset.groupby('class').size())
+
+dataset.plot(kind = "box", subplots= True, layout = (2,2), sharex = False, sharey = False,)
+plt.show()
